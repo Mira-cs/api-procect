1d16ce9b06c1c6be2980ea51fa3e0739b030daca
Damira K <khamzinadamira@gmail.com>
2023-07-05 00:02:30 +1000

    User model created

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import json
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
@@ -9,56 +9,66 @@ print(app.config)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://renovations_app_dev:codernewapp2023@localhost:5432/renovation_materials'
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 print(db.__dict__)
 
 class User(db.Model):
   __tablename__ = 'users'
+  
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(30))
   last_name = db.Column(db.String(30))
-  email = db.Column
-  password = 
+  email = db.Column(db.String, nullable=False, unique=True)
+  password = db.Column(db.String, nullable=False)
   phone_number = db.Column(db.Integer)
+  is_shop_owner = db.Column(db.Boolean, default=False)
   
+class UserSchema(ma.Schema):
+  class Meta:
+    # listing the fields we want to include 
+    fields = ('id','name','email')
+    
 class Review(db.Model):
   __tablename__ = 'reviews'
+  
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(30))
   comment = db.Column(db.Text())
-  rating = db.Column(db.Integer())
+  rating = 
   material_id
   shop_id
   user_id
   
 class Material(db.Model):
-  __tablename__ = 'reviews'
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(30))
-  comment = db.Column(db.Text())
-  rating = db.Column(db.Integer())
-  material_id
-  shop_id
-  user_id
+  __tablename__ = 'materials'
+  
+  id = 
+  name = 
+  description = 
+  price = 
+  shop_id = 
+
   
 class Shop(db.Model):
-  __tablename__ = 'reviews'
+  __tablename__ = 'shops'
+  
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(30))
-  comment = db.Column(db.Text())
-  rating = db.Column(db.Integer())
-  material_id
-  shop_id
-  user_id
+  name = db.Column(db.String(30))
+  contact_info = 
+  address_id
 
 class Address(db.Model):
-  __tablename__ = 'reviews'
+  __tablename__ = 'addresses'
+  
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(30))
-  comment = db.Column(db.Text())
-  rating = db.Column(db.Integer())
-  material_id
-  shop_id
-  user_id
+  street_number =  
+  street_name = 
+  suburb = 
+  city  = 
+  state = 
+  zip_code = 
+  
 
 @app.cli.command('create')
 def create_db():
@@ -71,7 +81,7 @@ def seed_db():
   # Create an instance of the User model in memory
   users = [
     User(
-    name = 'Celeste'
+    name = 'Celeste',
     last_name = 'Adams'
    )
   ]
@@ -88,9 +98,9 @@ def seed_db():
 @app.route('/users')
 def all_users():  
 # select * from users;
-  stmt = db.select(User).where(User.name != "Celeste", User.id > 2).order_by(User.name)
+  stmt = db.select(User).order_by(User.name)
   users = db.session.scalars(stmt).all()
-  return users.dumps(users)
+  return UserSchema(many=True).dumps(users)
   
 
 @app.route('/')
