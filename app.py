2c57a7cf91ff375ad91c51c4eb843e72e92cb204
Damira K <khamzinadamira@gmail.com>
2023-07-05 13:52:45 +1000

    flask jwt extended was installed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 
@@ -113,34 +114,49 @@ def seed_db():
 
 @app.route('/register', methods=['POST'])
 def register(): 
-  # to validate and sanitize all the incoming data via Marshmallow schema
-  # Parse, sanitize and validate the incoming JSON data
-  # via schema
-  user_info = UserSchema().load(request.json)
-  # Create a new User instance with the schema data
-  user = User(
-    email=user_info['email'],
-    password=bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
-    name=user_info['name'],
-    last_name=user_info['last_name']
-  )
-  # Add and commit the new user
-  db.session.add(user)
-  db.session.commit()
-  # Return the new user, excluding the password
-  return UserSchema(exclude=['password']).dump(user), 201
+  try:
+    # to validate and sanitize all the incoming data via Marshmallow schema
+    # Parse, sanitize and validate the incoming JSON data
+    # via schema
+    user_info = UserSchema().load(request.json)
+    # Create a new User instance with the schema data
+    user = User(
+      email=user_info['email'],
+      password=bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+      name=user_info['name'],
+      last_name=user_info['last_name']
+    )
+    # Add and commit the new user
+    db.session.add(user)
+    db.session.commit()
+    
+    # Return the new user, excluding the password
+    return UserSchema(exclude=['password']).dump(user), 201
+  except IntegrityError:
+    return {'error': 'Email address already in use'}, 409
   
+@app.route('/login', methods=['POST'])
+def login():
+  try:
+    # to get the user from the database, where email equals to the email from the user data posted
+    stmt = db.select(User).filter_by(email=request.json['email'])
+    #  returning one result (scalar method, not scalars)
+    user = db.session.scalar(stmt)
+    # checking if the user exists (user=True) and password is correct,
+    # first parameter is users password from database,
+    # the second one is the one retrieved from the POST method)
+    if user and bcrypt.check_password_hash(user.password, request.json['password']):
+      return UserSchema(exclude=['password']).dump(user)
+    else:
+      return {'error': 'Invalid email address or password'}, 401
+  except KeyError:
+    return{'error': 'Email and password are required'}, 400
   
-   
+
 # # select * from users;
 #   stmt = db.select(User).order_by(User.name)
 #   users = db.session.scalars(stmt).all()
 #   return UserSchema(many=True).dumps(users)
  
-
-@app.route('/')
-def index():
- return 'Hello World'
-
 if __name__ == '__main__':
   app.run(debug=True)
